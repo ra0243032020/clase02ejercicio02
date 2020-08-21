7015a6f3051fc1b40d7c3417bfa95f387ab210e8
ra0243032020 <69596926+ra0243032020@users.noreply.github.com>
2020-08-21 20:37:39 -0300

    INICIODEEJERCICIO03

---
+++ Profesor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.clase02.clase02ejercicio02;
+
+/**
+ *
+ * @author FAMILIA
+ */
+public class Profesor extends Persona{
+    
+    
+    
+}
