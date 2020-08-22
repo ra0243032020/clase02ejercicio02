0b4ef70c385cc2bdfa9f1d9f5616c616d76a414b
ra0243032020 <69596926+ra0243032020@users.noreply.github.com>
2020-08-21 22:29:37 -0300

    Update Profesor.java

--- Profesor.java
+++ Profesor.java
@@ -9,8 +9,82 @@ package com.unab.clase02.clase02ejercicio02;
  *
  * @author FAMILIA
  */
-public class Profesor extends Persona{
-    
-    
-    
+public class Profesor extends Persona {
+
+    private String Titulo;
+    private String Dedicacion;
+
+    public Profesor
+        (String Nombre, String Direccion, String FechaNC, String Genero, int Edad, String Titulo, String Dedicacion) {
+        this.Nombre = Nombre;
+        this.Direccion = Direccion;
+        this.FechaNC = FechaNC;
+        this.Genero = Genero;
+        this.Edad = Edad;
+        this.Titulo = Titulo;
+        this.Dedicacion = Dedicacion;
+
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+
+    public String getDedicacion() {
+        return Dedicacion;
+    }
+
+    public void setDedicacion(String Dedicacion) {
+        this.Dedicacion = Dedicacion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getFechaNC() {
+        return FechaNC;
+    }
+
+    public void setFechaNC(String FechaNC) {
+        this.FechaNC = FechaNC;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{" + "Nombre=" + Nombre + ", Direccion=" + Direccion + ", FechaNC=" + FechaNC + ", Genero=" + Genero + ", Edad=" + Edad + ", Titulo=" + Titulo + ", Dedicacion=" + Dedicacion + '}';
+    }
+
 }
